a762357fc9f75107ecbfe39dbb9977ba56bfe8ac
zaneran <zaneranhuang@gmail.com>
2017-02-17 18:37:51 -0500

    initial commit

---
+++ TagNew/app/src/main/java/zhuoranhuang/tagnew/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package zhuoranhuang.tagnew.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zhuoranhuang.tagnew.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
